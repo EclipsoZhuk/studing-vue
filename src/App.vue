016eeca033964a46a52b0c71a3571600ef4deed6
VladFront <vladik.gonchar2015@gmail.com>
2022-03-31 22:05:55 +0300

    start vue

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <h1>{{ title }}</h1>
+    <ButtonOne outlined @click="increments">Click me!</ButtonOne>
+    <DirectivesTest />
+</template>
+
+<script>
+import ButtonOne from './components/ButtonOne.vue';
+import DirectivesTest from './components/DirectivesTest.vue';
+
+export default {
+    name: 'App',
+    components: {
+        ButtonOne,
+        DirectivesTest,
+    },
+    data() {
+        return {
+            amountOfClick: 0,
+        };
+    },
+    computed: {
+        title() {
+            return `Amount of click ${this.amountOfClick}`;
+        },
+    },
+    methods: {
+        increments() {
+            this.amountOfClick += 1;
+        },
+    },
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ src/components/DirectivesTest.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-if="isVisible">
+        <h1>DirectivesTest</h1>
+        <ul>
+            <template v-for="fruit in fruits">
+                <li v-if="fruit === 'banana'" :key="fruit">{{ fruit }}</li>
+            </template>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DirectivesTest',
+    data() {
+        return {
+            isVisible: true,
+            fruits: ['kiwi', 'banana', 'apple'],
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
